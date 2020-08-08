4c38a6a208a575e00b1e31610c670e3ebf7992b7
Laasri Nadia <laasrinadiaa@gmail.com>
2020-08-08 16:30:56 +0100

    cat added

---
+++ index.js
@@ -0,0 +1,29 @@
+const catBtn = document.querySelector('.cat-btn')
+const catLegs = document.querySelectorAll('.cat-leg')
+const scratches = document.querySelectorAll('.scratch')
+var snd = new Audio("scratch.mp3");
+
+
+catBtn.addEventListener('click', () => {
+    catLegs.forEach(catLeg => {
+        catLeg.classList.add("cat-leg-move");
+        // catLeg.classList.toggle('cat-leg-move')
+    })
+    scratches.forEach(scratch => {
+        scratch.classList.add("scratch-move")
+        // scratch.classList.toggle("scratch-move")
+    })
+    snd.play()
+})
+
+catLegs.forEach(catLeg => catLeg.addEventListener("transitionend", returnBackTheCat))
+
+function returnBackTheCat() {
+    snd.pause()
+    catLegs.forEach(catLeg => {
+        catLeg.classList.remove("cat-leg-move");
+    })
+    scratches.forEach(scratch => {
+        scratch.classList.remove("scratch-move")
+    })
+}
